aa70e4044af153ef404052e5a2ca0880c036d580
Pascal Riesinger <mail@pascal-riesinger.de>
2024-08-14 22:24:52 +0200

    Add route for capture page

--- server/app.py
+++ server/app.py
@@ -43,6 +43,11 @@ def debug():
     )
 
 
+@app.route("/capture")
+def capture():
+    return render_template("capture.html")
+
+
 @app.post("/api/v1/debug/reload")
 def reload_phones():
     socketio.emit("reload")
